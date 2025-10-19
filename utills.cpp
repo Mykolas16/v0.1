b49a98bc81b8b0449d2a85cab4f9ded25dfbcef9
Mykolas Gargun <mykolas.gargun@vvk.lt>
2025-10-19 15:14:17 +0300

    utills.cpp
    Signed-off-by: Mykolas Gargun <mykolas.gargun@vvk.lt>

---
+++ utills.cpp
@@ -0,0 +1,10 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <vector>
+#include <string>
+#include "Studentas.h"
+
+std::vector<Studentas> nuskaitytiIsFailo(const std::string& failoVardas);
+
+#endif
